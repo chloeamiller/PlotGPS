95d0148af575c33a4709302742f3fe9282153195
Chloe Miller <110626472+chloeamiller@users.noreply.github.com>
2022-08-09 13:33:06 -0700

    Update plotgps_graphsmaps.R

--- plotgps_graphsmaps.R
+++ plotgps_graphsmaps.R
@@ -17,7 +17,7 @@
 
 # SET WORKING DIRECTORY -----------------------------
 cat("SETTING WORKING DIRECTORY...\n\n", sep = "")
-wd <- "~/PlotGPS"
+wd <- "~/PlotGPSfolder"
 setwd(wd)
 cat("WORKING DIRECTORY HAS BEEN SET TO: ", wd, sep = "")
 
